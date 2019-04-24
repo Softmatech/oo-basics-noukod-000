e42e010d5a0451033bf67903a242c5753da41b5f
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-24 16:23:15 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -18,7 +18,7 @@ class Book
       def page_count=(num)
           @page_count = num
         end
-        
+
       def page_count
           @page_count
         end
@@ -26,7 +26,7 @@ class Book
         def genre=(genre)
     @genre = genre
   end
- 
+
   def genre
     @genre
   end
